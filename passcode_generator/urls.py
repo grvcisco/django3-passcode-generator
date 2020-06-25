b5d6764d21efc2abf2e9c5d2e02f1a802952f8cb
Gaurav Srivastava <er.gauravsri@gmail.com>
2020-06-25 12:39:14 +0530

    Changes in code

--- passcode_generator/urls.py
+++ passcode_generator/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from generator import views
+from generator import views # Added custom
 
 urlpatterns = [
     path('', views.home),
